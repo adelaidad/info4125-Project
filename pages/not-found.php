d4f10f4863db24040a7db7f720e8c71ff5ea6187
adelaidad <142744599+adelaidad@users.noreply.github.com>
2024-10-10 20:13:25 +0000

    added 404 page

---
+++ pages/not-found.php
@@ -0,0 +1,25 @@
+<?php
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>INFO 4125 PROJECT</title>
+
+    <link rel="stylesheet" type="text/css" href="/styles/site.css">
+</head>
+
+<body>
+
+  <main>
+    <h2>404 - Not Found</h2>
+
+    <p>The resource you're looking for doesn't seem to exist. Try checking the URLs in your code that sent you to this page.</p>
+  </main>
+
+</body>
+
+</html>
