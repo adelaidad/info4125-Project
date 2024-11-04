bab54fa1a6ae866e00ce91b62004606b0518eb49
adelaidad <142744599+adelaidad@users.noreply.github.com>
2024-11-04 01:55:46 +0000

    added the eatery menu

--- pages/eatery.php
+++ pages/eatery.php
@@ -8,7 +8,47 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     setcookie('protein', '', time() - 3600, "/");
   }
 
-  
+  if (isset($_POST['low_carb'])) {
+    setcookie('low_carb', '1', time() + (86400 * 30), "/");
+  } else {
+    setcookie('low_carb', '', time() - 3600, "/");
+  }
+
+  if (isset($_POST['low_cal'])) {
+    setcookie('low_cal', '1', time() + (86400 * 30), "/");
+  } else {
+    setcookie('low_cal', '', time() - 3600, "/");
+  }
+
+  if (isset($_POST['dairy_free'])) {
+    setcookie('dairy_free', '1', time() + (86400 * 30), "/");
+  } else {
+    setcookie('dairy_free', '', time() - 3600, "/");
+  }
+
+  if (isset($_POST['vegan'])) {
+    setcookie('vegan', '1', time() + (86400 * 30), "/");
+  } else {
+    setcookie('vegan', '', time() - 3600, "/");
+  }
+
+  if (isset($_POST['vegetarian'])) {
+    setcookie('vegetarian', '1', time() + (86400 * 30), "/");
+  } else {
+    setcookie('vegetarian', '', time() - 3600, "/");
+  }
+
+  if (isset($_POST['gluten_free'])) {
+    setcookie('gluten_free', '1', time() + (86400 * 30), "/");
+  } else {
+    setcookie('gluten_free', '', time() - 3600, "/");
+  }
+
+  if (isset($_POST['low_cholesterol'])) {
+    setcookie('low_cholesterol', '1', time() + (86400 * 30), "/");
+  } else {
+    setcookie('low_cholesterol', '', time() - 3600, "/");
+  }
 
   header("Location: " . $_SERVER['PHP_SELF']);
   exit();
@@ -33,6 +73,15 @@ ob_end_flush();
   <h2>Where do you want to eat?</h2>
 
   <a href="/meals?<?php echo http_build_query(array("eatery" => 2)); ?>" class='button'>Cafe Jennies</a>
+  <a href="/meals?<?php echo http_build_query(array("eatery" => 3)); ?>" class='button'>Crossings Café</a>
+  <a href="/meals?<?php echo http_build_query(array("eatery" => 4)); ?>" class='button'>Cornell Dairy Bar</a>
+  <a href="/meals?<?php echo http_build_query(array("eatery" => 5)); ?>" class='button'>The Big Red Barn</a>
+  <a href="/meals?<?php echo http_build_query(array("eatery" => 6)); ?>" class='button'>Mattin's</a>
+  <a href="/meals?<?php echo http_build_query(array("eatery" => 7)); ?>" class='button'>Goldie's</a>
+  <a href="/meals?<?php echo http_build_query(array("eatery" => 8)); ?>" class='button'>Atrium Café</a>
+  <a href="/meals?<?php echo http_build_query(array("eatery" => 9)); ?>" class='button'>Mann Café</a>
+  <a href="/meals?<?php echo http_build_query(array("eatery" => 10)); ?>" class='button'>Martha's</a>
+  <a href="/meals?<?php echo http_build_query(array("eatery" => 11)); ?>" class='button'>Risley Dinings</a>
   <!-- Add the rest of the eateries -->
 </body>
 
